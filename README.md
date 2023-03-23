cd034b6c829655da5c230d9e0960126a50875c4a
Alexandre Quercia <alquerci@email.com>
2023-03-23 21:30:08 +0100

    Add java skeleton

---
+++ README.md
@@ -0,0 +1,33 @@
+Start new kata
+--------------
+
+```
+bin/new vendor.package_name ClassName
+```
+
+Tests
+-----
+
+```
+bin/test
+```
+
+Code Coverage report [target/site/jacoco/index.html][]
+
+
+Format code
+-----------
+
+```
+bin/cs-fixer
+```
+
+
+Mutation Testing
+----------------
+
+```
+bin/test-mutation
+```
+
+Mutation test report [target/pit-reports/index.html][]
---
+++ tests/AcmeTest.java
@@ -0,0 +1,14 @@
+package acme.tests;
+
+// import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.params.ParameterizedTest;
+// import org.junit.jupiter.params.provider.CsvSource;
+
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class AcmeTest {
+    @Test
+    public void nothing() {}
+}
