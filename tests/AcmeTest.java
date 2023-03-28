2b17880278e592ca0e7e7a2bbe82a678c8046156
Alexandre Quercia <alquerci@email.com>
2023-03-28 21:48:11 +0200

    fixup! Add java skeleton

--- tests/AcmeTest.java
+++ tests/AcmeTest.java
@@ -4,6 +4,9 @@ package acme.tests;
 import org.junit.jupiter.api.Test;
 // import org.junit.jupiter.params.ParameterizedTest;
 // import org.junit.jupiter.params.provider.CsvSource;
+// import org.junit.jupiter.params.provider.MethodSource;
+// import org.junit.jupiter.params.provider.Arguments;
+// import java.util.stream.Stream;
 
 // import static org.junit.jupiter.api.Assertions.assertEquals;
 // import static org.junit.jupiter.api.Assertions.assertThrows;
